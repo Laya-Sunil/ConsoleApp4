70c90071bdb63a7f5856f60a31f6fbff8f63752e
Laya-Sunil <layasunil528@gmail.com>
2021-03-09 11:47:50 +0530

    Local commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static List<String> Names = new List<string> { "Zac", "Mara", "Scarlet", "Lilly", "Luke" };
+        static void Main(string[] args)
+        {
+            DisplayCount();
+            int c =DisplayNames();
+            TotalCount(c);
+            Console.ReadKey();
+        }
+        public static async Task DisplayCount()
+        {
+           
+            await Task.Run(() =>
+            {
+                foreach (var name in Names)
+                {
+
+                    Console.WriteLine(name);
+                    
+                    Task.Delay(150).Wait();
+                }
+
+            });
+            //TotalCount(count);
+        }
+
+
+        public static int DisplayNames()
+        {
+            int count = 0;
+            foreach (var item in Names)
+            {
+                count += 1;
+                //Console.WriteLine("Count = " + count);
+                Console.WriteLine("Counting");
+                
+                Task.Delay(150).Wait();
+            }
+            return count;
+        }
+        public static void TotalCount(int c)
+        {
+            Console.WriteLine("total count = " + c);
+        }
+    }
+}
+
